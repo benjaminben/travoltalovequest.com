9bcbd50c4f0c110e6bbf2564850d93a65a61735c
benBen <benmbenjamin@gmail.com>
2019-02-14 16:02:26 -0800

    full sequence draft 1

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,9 @@ import {makePlayerFromId} from './yt'
 
 var tag = document.createElement('script'),
     vidId = "CBc9uL4n6S0",
-    player
+    player,
+    playerLoaded,
+    windowLoaded
 
 tag.src = "https://www.youtube.com/iframe_api"
 var firstScriptTag = document.getElementsByTagName('script')[0]
@@ -12,17 +14,28 @@ window.onYouTubeIframeAPIReady = () => {
   player = makePlayerFromId(vidId, onPlayerReady, onPlayerStateChange)
 }
 
+window.addEventListener("load", () => {
+  windowLoaded = true
+  attemptLoaded()
+})
+
 document.getElementById("john")
 john.addEventListener("click", () => player.playVideo())
 
 function onPlayerReady(event) {
   console.log('player ready')
-  // document.querySelector("#vid").className += " ready"
-  loaded()
+  playerLoaded = true
+  attemptLoaded()
 }
 
 function onPlayerStateChange(event) {
   console.log('player state change', event.data)
+}
+
+function attemptLoaded() {
+  if (playerLoaded && windowLoaded) {
+    loaded()
+  }
 }
 
 function loaded() {

aff91bfddc189c9b054e8fae96692ec10453ade7
benBen <benmbenjamin@gmail.com>
2019-02-26 01:48:11 -0800

    mp4 fire and ga

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,42 @@
-var windowLoaded
+init()
 
-window.addEventListener("load", () => {
-  windowLoaded = true
-  attemptLoaded()
-})
-
-document.getElementById("john")
-
-function onPlayerReady(event) {
-  console.log('player ready')
-  playerLoaded = true
-  attemptLoaded()
+function init() {
+  const support = checkSupport()
+  if (!support.mp4) {useGif()}
+  window.addEventListener("load", () => {
+    loaded()
+  })
 }
 
 function onPlayerStateChange(event) {
   console.log('player state change', event.data)
 }
 
-function attemptLoaded() {
-  if (windowLoaded) {loaded()}
-}
-
 function loaded() {
   document.body.className = document.body.className.replace(/loading/, "")
+}
+
+function useGif() {
+  document.querySelector("img.fire").className += " active"
+  document.querySelector("video.fire").className += " none"
+}
+
+function checkSupport() {
+  var support = {}
+  var v = document.createElement('video');
+  if(v.canPlayType && v.canPlayType('video/mp4').replace(/no/, '')) {
+      support.mp4 = true;
+  }
+  return support
+}
+
+//LEGACY
+function onPlayerReady(event) {
+  console.log('player ready')
+  playerLoaded = true
+  attemptLoaded()
+}
+
+function attemptLoaded(win, player) {
+  if (win && player) {loaded()}
 }
